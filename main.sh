b626e267a9b83beb1e4bb8709aedb9fec0e6a8ff
Aleph <142549725+Prowi3@users.noreply.github.com>
2023-08-18 08:07:38 +0300

    Update main.sh

--- main.sh
+++ main.sh
@@ -20,5 +20,12 @@ izuku () {
 	  git_sync
 	  done
 }
-pip install -r requirements.txt
+pip install discord.py
+pip install random
+pip install os
+pip install io
+pip install httpx
+pip install requests
+pip install math
+pip install asyncio
 izuku
