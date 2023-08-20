130e239fa774245f006003d1f986e26c6a6571b0
Aleph <142549725+Prowi3@users.noreply.github.com>
2023-08-20 04:18:42 +0300

    a

--- src/main.py
+++ src/main.py
@@ -1,6 +1,7 @@
 import os
 import discord
 import io
+import random
 
 
 from discord.ext import commands
@@ -33,38 +34,91 @@ async def on_message(message):
 
 #snakes and ladders
 
+
+class SnakesAndLaddersGame:
+    def __init__(self):
+        self.grid_size = 10
+        self.square_size = 80
+        self.board_size = self.grid_size * self.square_size
+        self.players = [0, 0]
+        self.snakes = {16: 6, 47: 26, 49: 11, 56: 53, 62: 19, 64: 60, 87: 24, 93: 73, 95: 75, 98: 78}
+        self.ladders = {1: 38, 4: 14, 9: 31, 21: 42, 28: 84, 36: 44, 51: 67, 71: 91, 80: 100}
+        self.current_player = 0
+        self.dice_roll = 0
+
+    def roll_dice(self):
+        self.dice_roll = random.randint(1, 6)
+
+    def move_player(self, player_index):
+        self.players[player_index] += self.dice_roll
+        if self.players[player_index] in self.snakes:
+            self.players[player_index] = self.snakes[self.players[player_index]]
+        elif self.players[player_index] in self.ladders:
+            self.players[player_index] = self.ladders[self.players[player_index]]
+        if self.players[player_index] > 100:
+            self.players[player_index] = 200 - self.players[player_index]
+
+    def switch_turn(self):
+        self.current_player = 1 - self.current_player
+
+    def get_player_position(self, player_index):
+        row = self.players[player_index] // self.grid_size
+        col = self.players[player_index] % self.grid_size
+        return col, self.grid_size - row - 1
+
+    def play_turn(self):
+        self.roll_dice()
+        self.move_player(self.current_player)
+        self.switch_turn()
+
+    def is_game_over(self):
+        return any(player >= 100 for player in self.players)
+
 @bot.command()
 async def test(ctx):
-    img_size = 800
-    grid_size = 10
-    square_size = img_size // grid_size
+    game = SnakesAndLaddersGame()
 
-    img = Image.new("RGB", (img_size, img_size), color="white")
-    draw = ImageDraw.Draw(img)
+    while not game.is_game_over():
+        img = Image.new("RGB", (game.board_size, game.board_size), color="white")
+        draw = ImageDraw.Draw(img)
 
-    snakes = {16: 6, 47: 26, 49: 11, 56: 53, 62: 19, 64: 60, 87: 24, 93: 73, 95: 75, 98: 78}
-    ladders = {1: 38, 4: 14, 9: 31, 21: 42, 28: 84, 36: 44, 51: 67, 71: 91, 80: 100}
+        for i in range(game.grid_size):
+            for j in range(game.grid_size):
+                x = i * game.square_size
+                y = (game.grid_size - 1 - j) * game.square_size
+                square_coords = (x, y)
+                square_end_coords = (x + game.square_size, y + game.square_size)
+                draw.rectangle((square_coords, square_end_coords), outline="black", width=3)
 
-    for i in range(grid_size):
-        for j in range(grid_size):
-            x = i * square_size
-            y = (grid_size - 1 - j) * square_size  # Flip y-coordinate for correct orientation
-            square_coords = (x, y)
-            square_end_coords = (x + square_size, y + square_size)
-            draw.rectangle((square_coords, square_end_coords), outline="black", width=3)
+        for start, end in game.snakes.items():
+            start_x, start_y = game.get_player_position(start)
+            end_x, end_y = game.get_player_position(end)
+            draw.line([(start_x * game.square_size + game.square_size // 2, start_y * game.square_size + game.square_size // 2),
+                       (end_x * game.square_size + game.square_size // 2, end_y * game.square_size + game.square_size // 2)],
+                      fill="red", width=5)
+        
+        for start, end in game.ladders.items():
+            start_x, start_y = game.get_player_position(start)
+            end_x, end_y = game.get_player_position(end)
+            draw.line([(start_x * game.square_size + game.square_size // 2, start_y * game.square_size + game.square_size // 2),
+                       (end_x * game.square_size + game.square_size // 2, end_y * game.square_size + game.square_size // 2)],
+                      fill="green", width=5)
 
-            position = i + j * grid_size + 1  # Calculate the current position on the board
-            if position in snakes:
-                draw.text((x + square_size // 2, y + square_size // 2), "S", fill="red", font=None, anchor="mm")
-            if position in ladders:
-                draw.text((x + square_size // 2, y + square_size // 2), "L", fill="green", font=None, anchor="mm")
+        for i in range(2):
+            player_x, player_y = game.get_player_position(i)
+            draw.ellipse([(player_x * game.square_size + game.square_size // 4, player_y * game.square_size + game.square_size // 4),
+                         (player_x * game.square_size + 3 * game.square_size // 4, player_y * game.square_size + 3 * game.square_size // 4)],
+                         fill="blue" if i == game.current_player else "orange", outline="black", width=3)
 
-    img_path = "snakes_and_ladders.png"
-    img.save(img_path)
+        img_path = "snakes_and_ladders.png"
+        img.save(img_path)
 
-    with open(img_path, "rb") as img_file:
-        await ctx.send(file=discord.File(img_file))
+        await ctx.send("Current game state:", file=discord.File(img_path))
+        
+        game.play_turn()
 
+    winner = game.players.index(max(game.players)) + 1
+    await ctx.send(f"Player {winner} wins!")
 #error log
 
 @bot.event
