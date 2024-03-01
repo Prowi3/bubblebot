03438fa6e1e59d002c4bd0c1ae72f930d57e5ab3
Prowi3 <omarkj63@gmail.com>
2024-03-01 03:04:38 +0300

    cog goes brrr

--- src/commands/slash/rcat.py
+++ src/commands/slash/rcat.py
@@ -1,16 +1,20 @@
 from discord.ext import commands
 import httpx
 
-@commands.slash_command(name="cta", description="Send a random cat picture")
-async def random_cat(ctx):
-    async with httpx.AsyncClient() as client:
-        response = await client.get("https://api.thecatapi.com/v1/images/search")
-        if response.status_code == 200:
-            data = response.json()
-            cat_url = data[0]["url"]
-            await ctx.send(content=cat_url)
-        else:
-            await ctx.send("Failed to find cat :(")
+class RandomCat(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+
+    @commands.slash_command(name="cta", description="Send a random cat picture")
+    async def random_cat(self, ctx):
+        async with httpx.AsyncClient() as client:
+            response = await client.get("https://api.thecatapi.com/v1/images/search")
+            if response.status_code == 200:
+                data = response.json()
+                cat_url = data[0]["url"]
+                await ctx.send(content=cat_url)
+            else:
+                await ctx.send("Failed to find cat :(")
 
 def setup(bot):
-    bot.add_command(random_cat)
+    bot.add_cog(RandomCat(bot))
