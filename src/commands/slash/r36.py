e0b8c329efe83a86acb08478ea1d4aaa3bbbbc9e
Prowi3 <omarkj63@gmail.com>
2024-03-02 01:32:24 +0300

    nvm

--- src/commands/slash/r36.py
+++ src/commands/slash/r36.py
@@ -1,14 +1,13 @@
 import discord
 from discord.ext import commands
 import requests
-
-sent_media_links = []
+import xml.etree.ElementTree as ET
 
 class R34(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
-    @commands.slash_command(name="r36", description="Fetch images or GIFs from R34")
+    @commands.slash_command(name="r36", description="Fetch images from R34")
     async def r36(self, ctx, tag: str):
         if not ctx.channel.is_nsfw():
             await ctx.respond("This command can only be used in NSFW channels.", ephemeral=True)
@@ -22,29 +21,29 @@ class R34(commands.Cog):
         response = requests.get(url)
 
         if response.status_code == 200 and response.content:
-            data = response.json()
+            data = response.text
 
-            media = [post['file_url'] for post in data if post['file_url'].endswith(('.jpg', '.jpeg', '.png', '.gif')) and post['file_url'] not in sent_media_links]
+            root = ET.fromstring(data)
 
-            if media:
-                file_url = media[0]
-                is_gif = file_url.endswith('.gif')
-                if is_gif:
-                    message = f"You searched for {tag}. Here's a GIF:"
+            posts = root.findall('.//post')
+
+            if posts:
+                file_url = posts[0].get('file_url')
+
+                if file_url.endswith(".mp4"):
+                    await ctx.respond(f"[You searched for {tag}. Here's a video:]({file_url})")
                 else:
-                    message = f"You searched for {tag}. Here's an image:"
+                    embed = discord.Embed(
+                        title=f"You searched for {tag}.",
+                        color=discord.Colour(0x9FC6F6)
+                    )
+                    embed.set_image(url=file_url)
 
-                embed = discord.Embed(
-                    title=message,
-                    color=0x9FC6F6
-                )
-                embed.set_image(url=file_url)
-                await ctx.respond(embed=embed)
-                sent_media_links.append(file_url)
+                    await ctx.respond(embed=embed)
             else:
-                await ctx.respond(f"All media found for the provided tag ({tag}) have been sent or there are no media available.", ephemeral=True)
+                await ctx.respond(f"No images found for the provided tag ({tag}).", ephemeral=True)
         else:
-            await ctx.respond(f"Error fetching media. Status code: {response.status_code}", ephemeral=True)
+            await ctx.respond(f"Error fetching images. Status code: {response.status_code}", ephemeral=True)
 
 def setup(bot):
     bot.add_cog(R34(bot))
