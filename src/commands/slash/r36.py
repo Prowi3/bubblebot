485b43470d5e252597fb5d468eed1757a040bec2
Prowi3 <omarkj63@gmail.com>
2024-03-02 01:39:12 +0300

    mewo

--- src/commands/slash/r36.py
+++ src/commands/slash/r36.py
@@ -2,10 +2,12 @@ import discord
 from discord.ext import commands
 import requests
 import xml.etree.ElementTree as ET
+import random
 
 class R34(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
+        self.sent_urls = set()
 
     @commands.slash_command(name="r36", description="Fetch images from R34")
     async def r36(self, ctx, tag: str):
@@ -28,7 +30,16 @@ class R34(commands.Cog):
             posts = root.findall('.//post')
 
             if posts:
-                file_url = posts[0].get('file_url')
+                new_posts = [post for post in posts if post.get('file_url') not in self.sent_urls]
+
+                if not new_posts:
+                    await ctx.respond("All available images for this tag have already been sent.", ephemeral=True)
+                    return
+
+                random_post = random.choice(new_posts)
+                file_url = random_post.get('file_url')
+
+                self.sent_urls.add(file_url)
 
                 if file_url.endswith(".mp4"):
                     await ctx.respond(f"[You searched for {tag}. Here's a video:]({file_url})")
