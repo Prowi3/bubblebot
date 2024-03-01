3863b6045aac5263d270a7a961bf7dd5ca1e2877
Prowi3 <omarkj63@gmail.com>
2024-03-01 03:01:21 +0300

    wuh?

--- src/commands/slash/rcat.py
+++ src/commands/slash/rcat.py
@@ -1,16 +1,16 @@
-import httpx
 from discord.ext import commands
+import httpx
 
-@commands.slash_command(name="cta", description="Send a random cta picture")
-async def rcat(ctx):
+@commands.slash_command(name="cta", description="Send a random cat picture")
+async def random_cat(ctx):
     async with httpx.AsyncClient() as client:
         response = await client.get("https://api.thecatapi.com/v1/images/search")
         if response.status_code == 200:
             data = response.json()
-            cta_url = data[0]["url"]
-            await ctx.send(content=cta_url)
+            cat_url = data[0]["url"]
+            await ctx.send(content=cat_url)
         else:
-            await ctx.send("Failed to find cta :(")
+            await ctx.send("Failed to find cat :(")
 
 def setup(bot):
-    bot.add_command(rcat)
+    bot.add_slash_command(random_cat)
