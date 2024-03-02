6c526087437ba349b61b94c9bc1e0260f1c2ae63
Prowi3 <omarkj63@gmail.com>
2024-03-02 10:01:26 +0300

    image v4

--- src/commands/slash/sfw.py
+++ src/commands/slash/sfw.py
@@ -8,47 +8,16 @@ class Image(commands.Cog):
         self.bot = bot
         self.image = discord.SlashCommandGroup("image", "Get random images from yande.re")
 
-    @image.command(
-        name="sfw",
-        description="Get a random SFW image from yande.re"
-    )
-    async def sfw(self, ctx):
-        await ctx.defer()
-
-        random_page = random.randint(1, 100)
-
-        url = f"https://yande.re/post.json?tags=rating:safe&limit=0&page={random_page}"
-        response = requests.get(url)
-
-        if response.status_code == 200 and response.content:
-            data = response.json()
-
-            if data:
-                file_url = data[0]['sample_url']
-
-                embed = discord.Embed(
-                    title="Random SFW Image",
-                    colour=discord.Colour(0x9FC6F6)
-                )
-                embed.set_image(url=file_url)
-
-                await ctx.respond(embed=embed)
-            else:
-                await ctx.respond("No images found.", ephemeral=True)
-        else:
-            await ctx.respond(f"Error fetching images. Status code: {response.status_code}", ephemeral=True)
-
-    @image.command(
-        name="nsfw",
-        description="Get a random NSFW image from yande.re"
-    )
-    async def nsfw(self, ctx):
-        if ctx.channel.is_nsfw():
+        @self.image.command(
+            name="sfw",
+            description="Get a random SFW image from yande.re"
+        )
+        async def sfw(self, ctx):
             await ctx.defer()
 
             random_page = random.randint(1, 100)
 
-            url = f"https://yande.re/post.json?tags=rating:explicit&limit=0&page={random_page}"
+            url = f"https://yande.re/post.json?tags=rating:safe&limit=0&page={random_page}"
             response = requests.get(url)
 
             if response.status_code == 200 and response.content:
@@ -58,7 +27,7 @@ class Image(commands.Cog):
                     file_url = data[0]['sample_url']
 
                     embed = discord.Embed(
-                        title="Random NSFW Image",
+                        title="Random SFW Image",
                         colour=discord.Colour(0x9FC6F6)
                     )
                     embed.set_image(url=file_url)
@@ -68,8 +37,39 @@ class Image(commands.Cog):
                     await ctx.respond("No images found.", ephemeral=True)
             else:
                 await ctx.respond(f"Error fetching images. Status code: {response.status_code}", ephemeral=True)
-        else:
-            await ctx.respond("This command can only be used in NSFW channels.", ephemeral=True)
+
+        @self.image.command(
+            name="nsfw",
+            description="Get a random NSFW image from yande.re"
+        )
+        async def nsfw(self, ctx):
+            if ctx.channel.is_nsfw():
+                await ctx.defer()
+
+                random_page = random.randint(1, 100)
+
+                url = f"https://yande.re/post.json?tags=rating:explicit&limit=0&page={random_page}"
+                response = requests.get(url)
+
+                if response.status_code == 200 and response.content:
+                    data = response.json()
+
+                    if data:
+                        file_url = data[0]['sample_url']
+
+                        embed = discord.Embed(
+                            title="Random NSFW Image",
+                            colour=discord.Colour(0xFF0000)
+                        )
+                        embed.set_image(url=file_url)
+
+                        await ctx.respond(embed=embed)
+                    else:
+                        await ctx.respond("No images found.", ephemeral=True)
+                else:
+                    await ctx.respond(f"Error fetching images. Status code: {response.status_code}", ephemeral=True)
+            else:
+                await ctx.respond("This command can only be used in NSFW channels.", ephemeral=True)
 
 def setup(bot):
     bot.add_cog(Image(bot))
