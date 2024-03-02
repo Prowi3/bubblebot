ebb3708eb7c53f1033eac7d79bf5d6054e4ec322
Prowi3 <omarkj63@gmail.com>
2024-03-02 12:17:30 +0300

    a

--- src/commands/slash/draw_noise.py
+++ src/commands/slash/draw_noise.py
@@ -18,7 +18,7 @@ class DrawNoise(commands.Cog):
 
     @commands.slash_command(name="draw_noise", description='Draw an image using Noise')
     async def draw_noise(self, ctx: discord.ApplicationContext,
-        font: discord.Option(str, description="Select a font.", choices=fonts) = None, *,
+        font: discord.Option(str, description="Select a font.", choices=fonts), *,
         text: discord.Option(str = '',  description="Enter your text here. emojis and emots won't work."),
         octaves: discord.Option(int = 1, description="Choose the octave amount, the default is 1.0 (/help for more info)"), 
         lacunarity: discord.Option(float = 5.0, description="Choose the lacunarity amount, the default is 5.0 (/help for more info)"),
