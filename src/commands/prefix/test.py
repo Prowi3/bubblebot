7e64ef8513959f072043b1721f5e842aac418ebe
Prowi3 <omarkj63@gmail.com>
2024-03-03 08:35:07 +0300

    modified:   src/commands/prefix/test.py

--- src/commands/prefix/test.py
+++ src/commands/prefix/test.py
@@ -2,7 +2,7 @@ from discord.ext import commands
 
 @commands.command()
 async def test(ctx):
-    await ctx.send("This Command is from a separate file")
+    await ctx.send("12")
 
 def setup(bot):
     bot.add_command(test)
