55449a7f8800da9a2e480d400440725878b68f9b
Prowi3 <omarkj63@gmail.com>
2024-03-03 08:52:30 +0300

    very goofy

--- src/commands/prefix/status.py
+++ src/commands/prefix/status.py
@@ -1,11 +1,11 @@
-import os,sys
+import os, sys
 import subprocess
 from discord.ext import commands
 
 @commands.command()
-async def update(ctx, folder_name: str):
+async def update(ctx, src: str):
     subprocess.run(['git', 'fetch', 'origin', 'main'], check=True)
-    subprocess.run(['git', 'checkout', 'origin/main', folder_name], check=True)
+    subprocess.run(['git', 'checkout', 'origin/main', src], check=True)
     await ctx.channel.send(f"Folder '{src}' updated. Rebooting...")
     os.execv(sys.executable, ['python'] + sys.argv)
 
