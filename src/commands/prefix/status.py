4b0661feba53a19318c5ff72babf618ca38e03cd
Prowi3 <omarkj63@gmail.com>
2024-03-03 08:41:39 +0300

    hm

--- src/commands/prefix/status.py
+++ src/commands/prefix/status.py
@@ -1,9 +1,12 @@
 import os, sys
+import subprocess
 from discord.ext import commands
 
 @commands.command()
 async def update(ctx):
-    await ctx.channel.send("Rebooting...")
+    await ctx.channel.send("Updating...")
+    subprocess.run(['git', 'pull'], check=True)
+    await ctx.channel.send("Update complete. Rebooting...")
     os.execv(sys.executable, ['python'] + sys.argv)
 
 def setup(bot):
