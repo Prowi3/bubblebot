9fad276a0b623a2eee6fb2849b58f7498994bee7
Prowi3 <omarkj63@gmail.com>
2024-03-03 08:33:29 +0300

    modified:   src/commands/prefix/status.py

--- src/commands/prefix/status.py
+++ src/commands/prefix/status.py
@@ -1,10 +1,10 @@
-import sys
+import os, sys
 from discord.ext import commands
 
 @commands.command()
 async def update(ctx):
-    await ctx.channel.send("updating...")
-    sys.exit(0)
+    await ctx.channel.send("Rebooting...")
+    os.execv(sys.executable, ['python'] + sys.argv)
 
 def setup(bot):
     bot.add_command(update)
