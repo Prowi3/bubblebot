a0f8105025f0df2644287e6cd78b0d1f8b64921b
Prowi3 <omarkj63@gmail.com>
2024-03-04 07:30:15 +0300

    a

--- src/commands/slash/play_song.py
+++ src/commands/slash/play_song.py
@@ -8,7 +8,7 @@ class MusicPlayer(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
-    @commands.slash_command(name="play", description="Play a song from a YouTube URL")
+    @commands.slash_command(name="play_song", description="Play a song from a YouTube URL")
     async def play_song(self, ctx, url: str, channel: discord.VoiceChannel):
         try:
             yt = YouTube(url)
