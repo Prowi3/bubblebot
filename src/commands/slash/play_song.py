cd43146397015f91f8745fe2bc84babc4ae97123
Prowi3 <omarkj63@gmail.com>
2024-03-06 03:28:29 +0300

    oops :3

--- src/commands/slash/play_song.py
+++ src/commands/slash/play_song.py
@@ -12,7 +12,7 @@ class PlaySong(commands.Cog):
     @commands.slash_command(name="play_song", description="Download and play a song from a YouTube URL")
     async def play_song(self, ctx: discord.ApplicationContext,
                         url: str,
-                        channel: discord.Option(discord.ChannelType, description="Select the Voice Channel that you want to play the song", channel_type=discord.ChannelType.voice) = None, 
+                        channel: discord.Option(discord.ChannelType.voice, description="Select the Voice Channel that you want to play the song") = None, 
                         play: discord.Option(bool, description="Bubble will join the vc and play the song") = False, 
                         cancel: discord.Option(bool, description="Cancel the song that is Playing if any") = False, 
                         leave: discord.Option(bool, description="Stop the Song and Leave the channel") = False):
