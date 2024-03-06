2d92788d5c21a45503d5be57509f6fee0ec4e8cc
Prowi3 <omarkj63@gmail.com>
2024-03-06 03:40:54 +0300

    wuuh?

--- src/commands/slash/play_song.py
+++ src/commands/slash/play_song.py
@@ -9,7 +9,7 @@ class PlaySong(commands.Cog):
         self.bot = bot
         self.voice_client = None
 
-    @commands.slash_command(name="play_song", description="Download and play a song from a YouTube URL")
+    @commands.slash_command(name="wuuh", description="Download and play a song from a YouTube URL")
     async def play_song(self, ctx: discord.ApplicationContext,
                         url: str,
                         channel: discord.Option(discord.ChannelType.voice, description="Select the Voice Channel that you want to play the song") = None, 
@@ -70,4 +70,3 @@ class PlaySong(commands.Cog):
 
 def setup(bot):
     bot.add_cog(PlaySong(bot))
-    
