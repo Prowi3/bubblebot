8375fa7b8f92a3f92fe837ad206281dfab3f9075
Aleph <142549725+Prowi3@users.noreply.github.com>
2024-03-15 16:38:13 +0300

    test

--- src/commands/prefix/test.py
+++ src/commands/prefix/test.py
@@ -2,7 +2,7 @@ from discord.ext import commands
 
 @commands.command()
 async def test(ctx):
-    await ctx.send("15")
+    await ctx.send("it works yay!")
 
 def setup(bot):
     bot.add_command(test)
